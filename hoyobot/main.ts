1ba3f8e7a27aa9204482fd0601df2f1e9e8d2aa1
Daanish-Khan <dkhan045@uottawa.ca>
2023-09-17 19:44:45 -0400

    First command + command router

--- hoyobot/main.ts
+++ hoyobot/main.ts
@@ -1,13 +1,53 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env.secrets' });
-
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config({ path: '.env.secrets' });
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Load commands dynamically in commands folder
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+
+// Add command files to collection
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+// Command router
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while execuing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+client.login(process.env.BOT_SECRET);
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-
-client.login(process.env.BOT_SECRET);
