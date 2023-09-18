faf85e73bf1ba60572812f1be50b34979017fc7f
Daanish-Khan <dkhan045@uottawa.ca>
2023-09-18 10:41:00 -0400

    setup dotenv vault

--- hoyobot/deploy-commands.ts
+++ hoyobot/deploy-commands.ts
@@ -28,7 +28,7 @@ const rest = new REST().setToken(process.env.BOT_SECRET);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		await rest.put(
-			Routes.applicationGuildCommands(process.env.DEV_CLIENT_ID, process.env.DEV_GUILD_ID),
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.DEV_GUILD_ID),
 			{ body: commands },
 		);
 
