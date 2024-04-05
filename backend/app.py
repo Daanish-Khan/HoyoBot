b38cb3adf045c9116183eff9b81f9d48f5fd6cf4
Daanish-Khan <dkhan045@uottawa.ca>
2024-04-05 13:52:14 -0400

    fix bug

--- backend/app.py
+++ backend/app.py
@@ -38,10 +38,18 @@ async def challenge():
 			headers=HEADERS,
 		)
 		data = await r.json()
+		cookies = {cookie.key: cookie.value for cookie in r.cookies.values()}
+
+		if cookies:
+			_, code = check_in(cookies, response)
+			if code == 200:
+				return {"checkin": True}, 200
+
 		if data['retcode'] == -3101:
 			aigis = json.loads(r.headers['x-rpc-aigis'])
 			aigis['data'] = json.loads(aigis['data'])
 			return aigis, 200
+		
 	return data, 400
 
 @app.route('/discordauth', methods = ['POST'])
@@ -55,9 +63,8 @@ async def discordauth():
 @app.route('/login', methods = ['POST'])
 async def login():
 	response = supabase.table('decrypted_users').select('decrypted_username, decrypted_password, discord_id').eq('id', request.get_json()['account_id']).execute()
-	checkinResponse = ''
 	async with aiohttp.ClientSession() as session:
-		
+
 		r = await session.post(
 			'https://sg-public-api.hoyolab.com/account/ma-passport/api/webLoginByPassword',
 			json={
@@ -74,22 +81,37 @@ async def login():
 		data = await r.json()
 
 		cookies = {cookie.key: cookie.value for cookie in r.cookies.values()}
-		
+
+		if not data['data']:
+			return data, 400 
+
+		if data['data'].get('stoken'):
+			cookies['stoken'] = data['data']['stoken']
+
+		return await check_in(cookies, response)
+
+@app.route('/registered_with_token', methods = ['GET'])
+async def registered_with_token():
+	response = supabase.table('tokens').select('*').eq('discord_id', request.args.get('discord_id')).execute()
+	if not response.data:
+		return "False", 200
+	else:
+		return str(response.data[0].get('cookie_v1') != None), 200
+	
+async def check_in(cookies, response):
+	checkinResponse = ''
+
+	async with aiohttp.ClientSession() as session:
 		checkin = await session.post(
-			'https://sg-public-api.hoyolab.com/event/luna/os/sign',
-			json={ 'act_id': 'e202303301540311' },
-			headers={
-				'Cookie': 'account_id_v2=' + cookies['account_id_v2'] + ';account_mid_v2=' + cookies['account_mid_v2'] + ';cookie_token_v2=' + cookies['cookie_token_v2'] + ';ltoken_v2=' + cookies['ltoken_v2'] + ';ltuid_v2=' + cookies['ltuid_v2'], 
-				'User-Agent': 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36' 
-			}
-		)
+				'https://sg-public-api.hoyolab.com/event/luna/os/sign',
+				json={ 'act_id': 'e202303301540311' },
+				headers={
+					'Cookie': 'account_id_v2=' + cookies['account_id_v2'] + ';account_mid_v2=' + cookies['account_mid_v2'] + ';cookie_token_v2=' + cookies['cookie_token_v2'] + ';ltoken_v2=' + cookies['ltoken_v2'] + ';ltuid_v2=' + cookies['ltuid_v2'], 
+					'User-Agent': 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36' 
+				}
+			)
+		
 		checkinResponse = await checkin.json()
-
-	if not data['data']:
-		return data, 400 
-
-	if data['data'].get('stoken'):
-		cookies['stoken'] = data['data']['stoken']
 
 	supabase.table('tokens').upsert({
 		"discord_id": response.data[0].get('discord_id'),
@@ -102,11 +124,3 @@ async def login():
 	}).eq('id', request.get_json()['account_id']).execute()
 
 	return checkinResponse, 200
-
-@app.route('/registered_with_token', methods = ['GET'])
-async def registered_with_token():
-	response = supabase.table('tokens').select('*').eq('discord_id', request.args.get('discord_id')).execute()
-	if not response.data:
-		return "False", 200
-	else:
-		return str(response.data[0].get('cookie_v1') != None), 200
