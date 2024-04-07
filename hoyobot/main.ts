a029647b7db1c06b76556479365e6d9ed3d7c8c0
Daanish-Khan <dkhan045@uottawa.ca>
2024-04-07 15:54:09 -0400

    more test logging

--- hoyobot/main.ts
+++ hoyobot/main.ts
@@ -67,6 +67,8 @@ client.once(Events.ClientReady, async c => {
 		checkInAllUsers(client);
 	});
 
+	console.log(secrets);
+
 	if (secrets.ENV !== 'production') {
 		console.log('NOT IN PRODUCTION - SKIPPING UPDATE NOTES');
 		console.log(`Ready! Logged in as ${c.user.tag}`);
